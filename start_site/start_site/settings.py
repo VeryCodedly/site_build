319790a97f5ece49769845e000678e30b32d8030
VeryCodedly <codedlyconnect@gmail.com>
2025-04-10 21:02:52 +0100

    Added domain to ALLOWED_HOSTS

--- start_site/start_site/settings.py
+++ start_site/start_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.verycodedly.com', 'verycodedly.com', '.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
