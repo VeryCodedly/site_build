d723ad46630fb58c782162741317abb0f34b528f
VeryCodedly <codedlyconnect@gmail.com>
2025-05-05 16:56:11 +0100

    Created react frontend

--- start_site/start_site/settings.py
+++ start_site/start_site/settings.py
@@ -31,6 +31,10 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['www.verycodedly.com', 'verycodedly.com', '.onrender.com', 'localhost', '127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://verycodedly.com",
+]
 
 # Application definition
 
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
