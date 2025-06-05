951a6e437b9eec8be6360f294f7916b6832df66d
VeryCodedly <codedlyconnect@gmail.com>
2025-06-05 18:38:32 +0100

    Updated URL routing to include 'codedly.urls' for API endpoints

--- start_site/start_site/urls.py
+++ start_site/start_site/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('codedly.urls')),
-    path('api/', include('contact.urls')),
+    path('api/', include('codedly.urls')),
 
 ]
