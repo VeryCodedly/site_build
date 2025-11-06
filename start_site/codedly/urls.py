939b2ef101130305146ac70dd02a98e5f8eb1472
VeryCodedly <codedlyconnect@gmail.com>
2025-11-06 19:48:17 +0100

    Modified app.urls file

--- start_site/codedly/urls.py
+++ start_site/codedly/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import api_home # contact_view
+from .views import api_home 
 
 urlpatterns = [
     path('', api_home, name='api_home'),
-    # path("contact/", contact_view, name="contact"),
     
 ]
