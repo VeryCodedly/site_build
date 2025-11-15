4b600c9b11e95427e1c100ce2e7e4a7c3c724a88
VeryCodedly <codedlyconnect@gmail.com>
2025-11-15 15:57:11 +0100

    Found error in LessonSerializer

--- start_site/codedly/serializers.py
+++ start_site/codedly/serializers.py
@@ -62,7 +62,7 @@ class PostSerializer(serializers.ModelSerializer):
 class LessonSerializer(serializers.ModelSerializer):
     previous_lesson = serializers.SerializerMethodField()
     next_lesson = serializers.SerializerMethodField()
-    course = serializers.CharField(read_only=True, source='course.name')
+    course = serializers.CharField(read_only=True, source='course')
 
     class Meta:
         model = Lesson
