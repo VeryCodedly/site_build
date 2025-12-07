a945ad8dc8df91b4a0de9dd71792b0e3312fe62c
VeryCodedly <codedlyconnect@gmail.com>
2025-12-07 22:54:52 +0100

    Removed signals file error

--- start_site/codedly/apps.py
+++ start_site/codedly/apps.py
@@ -4,6 +4,4 @@ from django.apps import AppConfig
 class CodedlyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'codedly'
-
-    def ready(self):
-        import codedly.signals  # ensure signals are loaded
+    verbose_name = "VeryCodedly"
